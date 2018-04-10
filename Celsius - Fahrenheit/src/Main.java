d7ebe08f2c580b03fbc3550371dad6af59c12d45
2IIZ <2IIZ1998@gmail.com>
2018-04-10 15:55:47 +0200

    Added comment plus loop(that don't work)

--- Celsius - Fahrenheit/src/Main.java
+++ Celsius - Fahrenheit/src/Main.java
@@ -8,52 +8,57 @@ public class Main {
         double degrees = 0;
         double degreesConverted = 0;
 
+        String stop;
+        String choice;
+
         System.out.println("Celsius - Fahrenheit Converter");
 
         Scanner sc = new Scanner(System.in);
 
-        do {
+        while (ok){
             System.out.println("In which Degrees will you input the data ?");
             System.out.println("F for Fahrenheit > Celsius");
             System.out.println("C for Celsius > Fahrenheit");
 
-            String choice = sc.nextLine();
+            choice = sc.nextLine();
 
             if (choice.equals("f") || choice.equals("F")) {
 
-                //F to C
+                //Fahrenheit to Celsius
                 System.out.println("Input the Fahrenheit data number : ");
                 degrees = sc.nextInt();
 
-                degreesConverted = degrees;
                 degreesConverted -= 32;
-                degreesConverted *= 5;
-                degreesConverted /= 9;
+                degreesConverted /= 1.8;
 
                 System.out.println(degrees +"º Fahrenheit = "+ degreesConverted +"º Celsius ");
 
-                ok = false;
-
             } else if (choice.equals("c") || choice.equals("C")) {
 
-                //C to F
+                //Celsius to Farhenheit
                 System.out.println("Input the Celsius data number : ");
                 degrees = sc.nextInt();
 
-                degreesConverted = degrees;     //Celsius to Fahrenheit
                 degreesConverted = degrees * 1.8;
                 degreesConverted += 32;
 
                 System.out.println(degrees +"º Celsius = "+ degreesConverted +"º Fahrenheit ");
 
-                ok = false;
-            } else {
+                } else {
                 System.out.println("You may had a misspelled letter, try again...");
             }
 
 
-        }while(ok);
+            System.out.println();
+            System.out.println("Do you want to try again ? y/n");
 
-        //relance ok = 1;
+            stop = sc.nextLine(); // this doesn't work, don't know why
+
+            if (stop.equals("y") || stop.equals("Y")) { ok = true; } else { ok = false; }
+
+        }
+
+
+
     }
 }
